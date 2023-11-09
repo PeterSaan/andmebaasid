b3a341a55b9472886a6163effc64e84b3b0903e2
PeterSaan <saanpeter@gmail.com>
2023-11-09 11:28:16 +0200

    Update delete.js

--- delete.js
+++ delete.js
@@ -1,6 +1,6 @@
 import { players, guardians, teams } from "./db/schema.js";
 import { db } from "./dbDrizzle.js";
 
-const del = await db.delete(players).where(players.name == "Markus Halliksoo");
+const del = await db.delete(players).where(eq(players.name, "Markus Halliksoo"));
 del ? console.log("Deleted") : console.log("There was a problem");
 process.exit();
