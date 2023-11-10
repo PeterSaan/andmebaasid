ad9cec7754ddc3607caaac35719659c0532816f6
PeterSaan <saanpeter@gmail.com>
2023-11-10 14:19:25 +0200

    Got delete to work

--- delete.js
+++ delete.js
@@ -1,6 +1,9 @@
+import { eq } from "drizzle-orm";
 import { players, guardians, teams } from "./db/schema.js";
 import { db } from "./dbDrizzle.js";
 
-const del = await db.delete(players).where(eq(players.name, "Markus Halliksoo"));
+const deletable = process.argv.splice(2, 2).join("");
+const del = await db.delete(players).where(eq(players.name, deletable));
+
 del ? console.log("Deleted") : console.log("There was a problem");
 process.exit();
