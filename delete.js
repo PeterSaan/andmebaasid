a8b0bac9f1b87beded7c0b7477ceacc799fe8b6d
PeterSaan <saanpeter@gmail.com>
2023-11-21 22:48:14 +0200

    Made the delete script more usable via terminal

--- delete.js
+++ delete.js
@@ -2,14 +2,8 @@ import { eq } from "drizzle-orm";
 import { players, guardians, teams } from "./db/schema.js";
 import { db } from "./dbDrizzle.js";
 
-const deletable = process.argv.splice(2, 2).join("");
-const del = await db.delete(players).where(eq(players.name, "petsmees"));
-
-if(del) {
-    for(let i = 1; i < (Object.keys(players).length); i++) {
-
-    }
-}
-
+let nrOfNames = (process.argv).length - 2
+const deletable = process.argv.splice(2, nrOfNames);
+await db.delete(players).where(eq(players.name, deletable));
 
 process.exit();
