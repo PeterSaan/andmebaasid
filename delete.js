22649d1e0e2264baed594e6f7bdaf645c6077689
PeterSaan <saanpeter@gmail.com>
2023-11-21 11:36:12 +0200

    Trying to reset id after deleting

--- delete.js
+++ delete.js
@@ -6,9 +6,9 @@ const deletable = process.argv.splice(2, 2).join("");
 const del = await db.delete(players).where(eq(players.name, "petsmees"));
 
 if(del) {
-    console.log("The player was deleted successfully");
-} else {
-    console.log("Something went wrong");
+    for(let i = 1; i < (Object.keys(players).length); i++) {
+
+    }
 }
 
 
